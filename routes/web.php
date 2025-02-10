6d065e993e08f48d45a0f75f202794c65e4ba076
Moh Noor Al Azam <noor.azam@narotama.ac.id>
2025-02-10 13:30:48 +0700

    update route web

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-//	echo "OK";
-	return view('hello');
-});
-
+Route::get('/', [App\Http\Controllers\LandingPage::class, 'index']);
 Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
