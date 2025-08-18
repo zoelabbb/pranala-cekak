8a83fb303df560eaada56769cda7ea91917d87cb
Moh Noor Al Azam <noor.azam@narotama.ac.id>
2025-08-18 10:20:29 +0700

    update routing for auth

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/_admin', [App\Http\Controllers\HomeController::class, 'index'])->name('admin');
 
 Route::get('/', [App\Http\Controllers\LandingPage::class, 'index']);
 Route::get('/{uri}', [App\Http\Controllers\LandingPage::class, 'index']);
-
 Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('admin');
