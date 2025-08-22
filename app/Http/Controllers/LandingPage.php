c401d93267f634e397d8ac46dc5ade80143e3cd3
Ryu-sama <zoelabbb@gmail.com>
2025-08-22 10:32:35 +0700

    Add LandingPage controller and migration files for microsites and their contents

---
+++ app/Http/Controllers/LandingPage.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class LandingPage extends Controller
+{
+    public function index($uri = 0)
+    {
+        if ($uri == 0) // If no URI is provided, show the landing page
+        {
+            $logo = asset('images/logo.png');
+            return view('landing_page/index', compact('logo'));
+        }
+
+        // If a URI is provided, check if it exists in the redirects table
+        $uri = Str::of($uri)->trim('/')->toString(); // Remove leading and trailing slashes
+        $now = now();
+        $goesTo = DB::table('redirects')
+            ->where('active', true)
+            ->whereRaw('BINARY `uri` = ?', [(string) $uri])
+            ->where(function ($query) use ($now) {
+                $query->where('expired', '>', $now)
+                    ->orWhereNull('expired');
+            })
+            ->first();
+
+        if ($goesTo == null) // If no redirect found, show 404 page
+        {
+            $logo = asset('images/404.png');
+            $url = Str::of(url()->current())->toHtmlString();
+            return view('404/index', compact('logo', 'url'));
+        }
+
+        if ($goesTo->redirect)
+            return redirect()->away($goesTo->url); // Redirect to the URL if redirect is true
+        else {
+            $url = Str::of(url()->current())->toHtmlString();
+            $whichMicrosite = $goesTo = DB::table('microsites')
+                ->where('redirects_id', $goesTo->id)
+                ->first();
+            $namaAcara = $whichMicrosite->micro_name;
+            $waktuAcara = \Carbon\Carbon::parse($whichMicrosite->time)->locale('id')->translatedFormat('d F Y | H:i');
+            $lokasiAcara = $whichMicrosite->location;
+            $links = $this->micrositeContents($whichMicrosite->id);
+            //            print_r($whichMicrosite);
+            //            echo "<br>";
+            //            print_r($micrositeContents);
+            return view('microsite/index', compact('url', 'namaAcara', 'waktuAcara', 'lokasiAcara', 'links'));
+        }
+    }
+
+    private function micrositeContents($micrositesID)
+    {
+        return DB::table('microsites_contents')
+            ->where('microsites_id', $micrositesID)
+            ->orderBy('created_at', 'asc')
+            ->get();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+use App\Http\Controllers\LandingPage;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
+
+Route::get('/{uri?}', [LandingPage::class, 'index'])->name('landing.page');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
@@ -19,4 +22,4 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
